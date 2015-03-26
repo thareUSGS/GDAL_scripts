65869f43cf2164b88ba9966b4a243d2715e40da4
Trent Hare <thare@usgs.gov>
2015-03-26 08:32:51 -0700

    Simple run script to setup local environment for GDAL/scipy and to push to our cluster.

---
+++ gdal_baseline_slope/run_gdal_HiRISE_RoverSlope_crop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# add Anaconda 2.0.1 (for GDAL and scipy)
+export PATH="/work/users/thare/python/anaconda/bin:$PATH"
+
+
+echo "gdal_HiRISE_RoverSlope_crop.pl $1"  | msub  -V -S /bin/bash -d `pwd` -l walltime=72:00:00 -j oe -o LOG_baselineSlope.txt
+
+
