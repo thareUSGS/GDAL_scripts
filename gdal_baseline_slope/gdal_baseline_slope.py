421b3646ab1ed991ac51b7296ac5edd645b5a565
Trent Hare <thare@usgs.gov>
2015-04-03 14:26:19 -0700

    add requested to always support 32bit output even if type is set to 8bit

--- gdal_baseline_slope/gdal_baseline_slope.py
+++ gdal_baseline_slope/gdal_baseline_slope.py
@@ -233,10 +233,14 @@ if outType is None:
    outGdalType = inType
 else:
    outGdalType = ParseType(outType)
+   outfile8 = outfile
+   outfile = "32bit_"+outfile
 
 if crop:
    cropfile = outfile
    outfile = "temp_"+outfile
+   cropfile8 = outfile8
+   outfile8 = "temp_"+outfile8
 
 # Read geotransform matrix and calculate ground coordinates
 geomatrix = indataset.GetGeoTransform()
@@ -257,10 +261,16 @@ X5 = X - (cellsizeX / 2.0)
 Y5 = Y - (cellsizeY / 2.0)
 
 #define output format, name, size, type mostly based on input image
+#this is meant for 32bit ourput file
 out_driver = gdal.GetDriverByName(format)
 outdataset = out_driver.Create(outfile, indataset.RasterXSize, \
-             indataset.RasterYSize, indataset.RasterCount, outGdalType)
+             indataset.RasterYSize, indataset.RasterCount, inType)
 outdataset.SetProjection(indataset.GetProjection())
+
+if outType == 'Byte': 
+   outdataset8 = out_driver.Create(outfile8, indataset.RasterXSize, \
+             indataset.RasterYSize, indataset.RasterCount, outGdalType)
+   outdataset8.SetProjection(indataset.GetProjection())
 
 #loop over bands -- probably can handle all bands at once...
 for band in range (1, indataset.RasterCount + 1):
@@ -269,6 +279,8 @@ for band in range (1, indataset.RasterCount + 1):
    #   outNoData=iBand.GetNoDataValue()
    outNoData=iBand.GetNoDataValue()
    outband = outdataset.GetRasterBand(band)
+   if outType == 'Byte': 
+      outband8 = outdataset8.GetRasterBand(band)
 
    raster_data = iBand.ReadAsArray(0, 0, cols, rows)
    if baseline == 1:
@@ -277,38 +289,44 @@ for band in range (1, indataset.RasterCount + 1):
       #shift half a pixel to center 
       newGeomatrix = (X1 , geomatrix[1], geomatrix[2], Y1, geomatrix[4], geomatrix[5])
       outdataset.SetGeoTransform(newGeomatrix)
+      if outType == 'Byte': 
+         outdataset8.SetGeoTransform(newGeomatrix)
    elif baseline == 2:
       slope = generic_filter(raster_data, calc_slope_baseline2, size=3, mode='constant',
                        cval=outNoData, extra_arguments=(cellsizeX, cellsizeY, outNoData))
       outdataset.SetGeoTransform(indataset.GetGeoTransform())
+      if outType == 'Byte': 
+         outdataset8.SetGeoTransform(indataset.GetGeoTransform())
    elif baseline == 5:
       slope = generic_filter(raster_data, calc_slope_baseline5, size=6, mode='constant',
                        cval=outNoData, extra_arguments=(cellsizeX, cellsizeY, outNoData))
       #shift half a pixel to center 
       newGeomatrix = (X5 , geomatrix[1], geomatrix[2], Y5, geomatrix[4], geomatrix[5])
       outdataset.SetGeoTransform(newGeomatrix)
+      if outType == 'Byte': 
+         outdataset8.SetGeoTransform(newGeomatrix)
    else:
       slope = generic_filter(raster_data, calc_slope, size=3, mode='constant',
                        cval=outNoData, extra_arguments=(cellsizeX, cellsizeY, outNoData))
       outdataset.SetGeoTransform(indataset.GetGeoTransform())
+      if outType == 'Byte': 
+         outdataset8.SetGeoTransform(indataset.GetGeoTransform())
 
    #write out band to new file
+   outband.SetOffset(0)
+   outband.SetScale(1)
+   outband.SetNoDataValue(outNoData)
+   outband.WriteArray(slope)
+
+   #write out raster data
    if outType == 'Byte': #if Byte (8bit), scale slope degrees to 1 to 255).
       slope[slope == iBand.GetNoDataValue()] = np.nan
       slope_8bit = np.round((slope + 0.2) * 5.0)
       slope_masked = np.where(np.isnan(slope), 0, slope_8bit)
-      slope=slope_masked.view()
-      outband.SetOffset(-0.2)
-      outband.SetScale(0.2)
-      outband.SetNoDataValue(0) # should be 0
-   else:
-      outband.SetOffset(0)
-      outband.SetScale(1)
-      outband.SetNoDataValue(outNoData)
-
-   #write out raster data
-   outband.WriteArray(slope)
-
+      outband8.SetOffset(-0.2)
+      outband8.SetScale(0.2)
+      outband8.SetNoDataValue(0) # should be 0
+      outband8.WriteArray(slope_masked)
 
    if not quiet:
       print ("band: " + str(band) + " complete."),
@@ -321,8 +339,12 @@ if (crop and (baseline != 3)):
    newXSize = outdataset.RasterXSize - baseline
    newYSize = outdataset.RasterYSize - baseline
    cropdataset = out_driver.Create(cropfile, newXSize, newYSize, \
-                 outdataset.RasterCount, outGdalType)
+                 outdataset.RasterCount, inType)
    cropdataset.SetProjection(outdataset.GetProjection())
+   if outType == 'Byte': 
+      cropdataset8 = out_driver.Create(cropfile8, newXSize, newYSize, \
+                 outdataset.RasterCount, outGdalType)
+      cropdataset8.SetProjection(outdataset.GetProjection())
    print "cropping file with %d less pixels X=%d, Y=%d" % (baseline, newXSize, newYSize)
 
    # Read geotransform matrix and calculate ground coordinates
@@ -335,12 +357,18 @@ if (crop and (baseline != 3)):
    for band in range (1, outdataset.RasterCount + 1):
       iBand = outdataset.GetRasterBand(band)
       cropBand = cropdataset.GetRasterBand(band)
+      if outType == 'Byte': 
+         iBand8 = outdataset8.GetRasterBand(band)
+         cropBand8 = cropdataset8.GetRasterBand(band)
       if baseline == 1:
          #no shift just removeing pixel from right and bottom
          #raster_data = iBand.ReadAsArray(0, 0, cols - 1, rows - 1)
          cropBand.WriteArray(slope[1:,1:])
 	 newGeomatrix = (X , geomatrix[1], geomatrix[2], Y, geomatrix[4], geomatrix[5])
 	 cropdataset.SetGeoTransform(newGeomatrix)
+         if outType == 'Byte': 
+            cropBand8.WriteArray(slope_masked[1:,1:])
+	    cropdataset8.SetGeoTransform(newGeomatrix)
       elif baseline == 2:
          #shift 1 pixel in and remove pixel from right/bottom
          #raster_data = iBand.ReadAsArray(1, 1, cols - 1, rows - 1)
@@ -349,6 +377,9 @@ if (crop and (baseline != 3)):
          Y2 = Y + cellsizeY
          newGeomatrix = (X2 , geomatrix[1], geomatrix[2], Y2, geomatrix[4], geomatrix[5])
          cropdataset.SetGeoTransform(newGeomatrix)
+         if outType == 'Byte': 
+            cropBand8.WriteArray(slope_masked[1:-1,1:-1])
+            cropdataset8.SetGeoTransform(newGeomatrix)
       elif baseline == 5:
          #shift 2 pixel in and remove 2 pixels from right/bottom
          #raster_data = iBand.ReadAsArray(2, 2, cols - 3, rows - 3)
@@ -357,19 +388,31 @@ if (crop and (baseline != 3)):
          Y5 = Y + (cellsizeY * 2)
          newGeomatrix = (X5 , geomatrix[1], geomatrix[2], Y5, geomatrix[4], geomatrix[5])
          cropdataset.SetGeoTransform(newGeomatrix)
+         if outType == 'Byte': 
+            cropBand8.WriteArray(slope_masked[3:-2,3:-2])
+            cropdataset8.SetGeoTransform(newGeomatrix)
       else:
          print "This baseline not supported during a crop\n"
 
       cropBand.SetNoDataValue(iBand.GetNoDataValue())
       cropBand.SetScale(iBand.GetScale())
       cropBand.SetOffset(iBand.GetOffset())
+      if outType == 'Byte': 
+         cropBand8.SetNoDataValue(iBand8.GetNoDataValue())
+         cropBand8.SetScale(iBand8.GetScale())
+         cropBand8.SetOffset(iBand8.GetOffset())
 
    cropdataset = None
    outdataset = None
    os.remove(outfile)
+   if outType == 'Byte': 
+      cropdataset8 = None
+      outdataset8 = None
+      os.remove(outfile8)
     
 #set output to None to close file
 raster_data = None
 outdataset = None
+outdataset8 = None
 indataset = None
 
