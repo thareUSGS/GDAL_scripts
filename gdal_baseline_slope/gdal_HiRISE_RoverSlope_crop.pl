fec7335215bb346f066e647be836730364ebe20f
Trent Hare <thare@usgs.gov>
2015-04-08 12:05:49 -0700

    Updated to run stats on 32bit and hist on 8bit

--- gdal_baseline_slope/gdal_HiRISE_RoverSlope_crop.pl
+++ gdal_baseline_slope/gdal_HiRISE_RoverSlope_crop.pl
@@ -52,6 +52,8 @@
     # process baselines 
     ###############################################
     foreach (@baselines) {
+      $outSlope32  = "32bit_adir_".$root."_gdal_0".$_."_0".$_."_ang.tif";
+      $outSlope8  = "adir_".$root."_gdal_0".$_."_0".$_."_ang.tif";
       $outSlope  = "adir_".$root."_gdal_0".$_."_0".$_."_ang.tif";
       $outFigure = "adir_".$root."_gdal_0".$_."_0".$_."_ang_mag10_rb20.jpg";
       #if (-e $output) {
@@ -63,27 +65,45 @@
       print $cmd."\n";
       system($cmd);
   
-      #calculate stats on DEM. This helps the gdal_hist with good min/max values
+      #calculate stats on 8bit DEM. This helps the gdal_hist with good min/max values
       $cmd = "gdalinfo -stats $outSlope";
       print $cmd."\n";
       system($cmd);
   
-      #create histogram
       $hist = $root."_adir_".$_."m_hist.xls";
-      $cmd = "/usgs/cdev/contrib/bin/gdal_hist.py -unscale -stats $outSlope > $hist";
+      if (-e $outSlope32) {
+         #calculate stats on 32bit_DEM. This helps the gdal_hist with good min/max values
+         $cmd = "gdalinfo -stats $outSlope32";
+         print $cmd."\n";
+         system($cmd);
+
+         #create stats
+         #if 32bit file is created, create stats on that file
+         $cmd = "/usgs/cdev/contrib/bin/gdal_hist.py -unscale -stats $outSlope32 > $hist";
+         print $cmd."\n";
+         system($cmd);
+      } else {
+         #create stats
+         $cmd = "/usgs/cdev/contrib/bin/gdal_hist.py -unscale -stats $outSlope > $hist";
+         print $cmd."\n";
+         system($cmd);
+      }
+
+  
+      #append histogram in same output as above
+      #to make a more attract plot, we will create the histogram on the 8bit truncated version if available
+      $cmd = "/usgs/cdev/contrib/bin/gdal_hist.py -unscale -hist $outSlope >> $hist";
       print $cmd."\n";
       system($cmd);
 
-      #plot histogram
+      #plot histogram using Python's matplotlib
       $histPng  = $root."_adir_".$_."m_hist.png";
-
-
-      #old gnuplot method
-      #$cmd = "echo 'set terminal unknown; plot \"$hist\" using 2:3; set terminal jpeg; set output \"$histJpeg\"; set format y \"%.1t*10^%+02T\";  set arrow 1 nohead from 15,0 to 15,GPVAL_Y_MAX linewidth 1; set key top right;  set tics out; set tmargin 1; plot \"$hist\" using 2:3 with filledcurve lc rgb \"black\" fs solid 0.5 axes x1y1 title \"slope count\",  \"$hist\" using 2:4 lc rgb \"blue\" axes x1y2 title \"cumlative (0 to 100%)\" ' | gnuplot";
-
       $cmd = "/usgs/cdev/contrib/bin/slope_histogram_cumlative_graph.py -name \"$root 0$_\" $hist $histPng";
       print $cmd."\n";
       system($cmd);
+
+      #old gnuplot method
+      #$cmd = "echo 'set terminal unknown; plot \"$hist\" using 2:3; set terminal jpeg; set output \"$histJpeg\"; set format y \"%.1t*10^%+02T\";  set arrow 1 nohead from 15,0 to 15,GPVAL_Y_MAX linewidth 1; set key top right;  set tics out; set tmargin 1; plot \"$hist\" using 2:3 with filledcurve lc rgb \"black\" fs solid 0.5 axes x1y1 title \"slope count\",  \"$hist\" using 2:4 lc rgb \"blue\" axes x1y2 title \"cumlative (0 to 100%)\" ' | gnuplot";
 
   
       #resample to 10m/p using an average method for down-sampling
