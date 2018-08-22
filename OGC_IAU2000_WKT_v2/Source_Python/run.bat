8e3cc904883a42a84b9176933894790188967bf8
Trent Hare <thare@usgs.gov>
2018-08-21 17:35:07 -0700

    initial push for IAU 2015 report (minor updates to 2009 files)

--- OGC_IAU2000_WKT_v2/Source_Python/run.bat
+++ OGC_IAU2000_WKT_v2/Source_Python/run.bat
@@ -1,2 +1,3 @@
-python create_IAU2000_wkt_v2.py ../naifcodes_radii_m_wAsteroids_IAU2000.csv IAU2000_v2.wkt
-python create_IAU2000_wkt_v2.py ../naifcodes_radii_m_wAsteroids_IAU2009.csv IAU2009_v2.wkt
+REM python create_IAU2000_wkt_v3.py ../naifcodes_radii_m_wAsteroids_IAU2000.csv IAU2000_v2.wkt
+python create_IAU2000_wkt_v3.py ../naifcodes_radii_m_wAsteroids_IAU2009.csv IAU2009_v3.wkt
+python create_IAU2000_wkt_v3.py ../naifcodes_radii_m_wAsteroids_IAU2015.csv IAU2015_v3.wkt
